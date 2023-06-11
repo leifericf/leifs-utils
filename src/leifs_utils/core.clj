967396e70069483278e6bd743e908f1c9b4af998
Leif Eric Fredheim <463422+leifericf@users.noreply.github.com>
2023-06-11 12:16:29 +0200

    Cleaned up code a bit

--- src/leifs_utils/core.clj
+++ src/leifs_utils/core.clj
@@ -9,38 +9,36 @@
   [key]
   (key (edn/read-string (slurp "secrets.edn"))))
 
+(defn sh-out->json [shell-command]
+  (-> (process/sh shell-command)
+      :out
+      (json/parse-string true)))
+
 (defn get-project-data
   "Get projects for an Azure DevOps organization."
   [org-url]
-  (-> (process/sh "az devops project list --org" org-url)
-      :out
-      (json/parse-string true)
-      :value))
+  (:value (sh-out->json (str "az devops project list --org " org-url))))
 
 (defn get-project-repo-data
   "Get Git repos for a project in an Azure DevOps organization."
   [project-id]
-  (-> (process/sh "az repos list --project" project-id)
-      :out
-      (json/parse-string true)))
+  (sh-out->json (str "az repos list --project " project-id)))
 
 (defn get-all-repo-data
   "Get Git repos for an Azure DevOps organization."
   []
-  (let [org-url (get-secret :azure-devops-org-url)]
-    (->> org-url
-         (get-project-data)
-         (keep :id)
-         (map get-project-repo-data))))
+  (->> (get-secret :azure-devops-org-url)
+       (get-project-data)
+       (keep :id)
+       (map get-project-repo-data)))
 
 (defn clone-repo
   "Clone a Git repo to a destination path."
   ([repo-url]
    (clone-repo repo-url (str (file/home) (get-secret :repo-root-dir))))
   ([repo-url dest-path]
-   (let [path dest-path]
-     (if-not (file/exists? path) (file/create-dir path) nil)
-     (process/sh {:dir path} "git clone" repo-url))))
+   (if-not (file/exists? dest-path) (file/create-dir dest-path) nil)
+   (process/sh {:dir dest-path} "git clone" repo-url)))
 
 (defn clone-all-repos
   "Clone all Git repos from Azure DevOps."
