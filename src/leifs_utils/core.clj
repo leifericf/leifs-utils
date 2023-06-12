a64ca59d3abf3f7142abdcaa7e4db2e02ba70b1a
Leif Eric Fredheim <463422+leifericf@users.noreply.github.com>
2023-06-12 08:19:30 +0200

    Function names are sufficient as documentation

--- src/leifs_utils/core.clj
+++ src/leifs_utils/core.clj
@@ -9,24 +9,20 @@
   (key (edn/read-string (slurp "secrets.edn"))))
 
 (defn sh-out->json
-  "Heper function to call shell command and get output as JSON."
   [shell-command]
   (-> (process/sh shell-command)
       :out
       (json/parse-string true)))
 
 (defn get-project-data
-  "Get projects for an Azure DevOps organization."
   [org-url]
   (:value (sh-out->json (str "az devops project list --org " org-url))))
 
 (defn get-project-repo-data
-  "Get Git repos for a project in an Azure DevOps organization."
   [project-id]
   (sh-out->json (str "az repos list --project " project-id)))
 
 (defn get-all-repo-data
-  "Get Git repos for an Azure DevOps organization."
   []
   (->> (get-secret :azure-devops-org-url)
        (get-project-data)
@@ -34,7 +30,6 @@
        (run! get-project-repo-data)))
 
 (defn clone-repo
-  "Clone a Git repo to a destination path."
   ([repo-url]
    (clone-repo repo-url (str (file/home) (get-secret :repo-root-dir))))
   ([repo-url dest-path]
@@ -42,7 +37,6 @@
    (process/sh {:dir dest-path} "git clone" repo-url)))
 
 (defn clone-all-repos
-  "Clone all Git repos from Azure DevOps."
   []
   (->> (get-all-repo-data)
        (tree-seq coll? identity)
