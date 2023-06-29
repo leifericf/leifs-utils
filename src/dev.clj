478198849cd81bfa308d201c7474069ae90e3fdc
Teodor Heggelund <git@teod.eu>
2023-06-29 15:56:23 +0200

    Funksjon som laster avhengigheter uten å endre ns

--- src/dev.clj
+++ src/dev.clj
@@ -1,5 +1,12 @@
 (ns dev)
 
-(defn everything! []
+(defn everything!
+  "Laster alle symbolene som finnes inn i everything-navnerommet, og går dit"
+  []
   (require 'leifs-utils.everything)
   (in-ns 'leifs-utils.everything))
+
+(defn require-everything
+  "Laster inn \"alle tingene\" (ikke de sykliske avhengighetene)"
+  []
+  (require 'leifs-utils.everything))
