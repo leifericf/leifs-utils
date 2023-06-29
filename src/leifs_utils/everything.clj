2733a5db8f9ec6cbb586de118fce72f15231b105
Teodor Heggelund <git@teod.eu>
2023-06-29 15:01:22 +0200

    everything.clj

---
+++ src/leifs_utils/everything.clj
@@ -0,0 +1,11 @@
+(ns leifs-utils.everything
+  (:require
+   [leifs-utils.collection :refer :all]
+   [leifs-utils.file-system :refer :all]
+   [leifs-utils.git :refer :all]
+   [leifs-utils.settings :refer :all]
+   [leifs-utils.shell :refer :all]
+   [leifs-utils.time :refer :all]))
+
+;; Laster alt brutalt fra alle navnerom.
+;; Hvis du har kollisjoner i noen navnerom (to funksjoner i forskjellige navnerom heter det samme) kan du få problemer.
